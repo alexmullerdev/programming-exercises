8429a2b864b7c3e66d85de0600010f6d9e49702e
Alex Muller <alexmullercarvalho@gmail.com>
2026-03-16 21:28:03 -0300

    add c exercise ex011 do while

---
+++ c/ex011.c
@@ -0,0 +1,27 @@
+//Do While
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    do
+    {
+        printf("Digite um número par para sair do programa: \n");
+        scanf("%d", &numero);
+
+        if (numero % 2 == 0)
+        {
+            printf("%d é par!\n", numero);
+        }
+        else
+        {
+            printf("%d é ímpar!\n", numero);
+        }
+    } while (numero % 2 != 0);
+
+    printf("Saindo do programa...\n");
+
+    return 0;
+}
