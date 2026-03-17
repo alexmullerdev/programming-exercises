5d7927f791482435b27049ed9866639fddc95df4
Alex Muller <alexmullercarvalho@gmail.com>
2026-03-16 21:07:35 -0300

    add c exercise ex007 if else

---
+++ c/ex007.c
@@ -0,0 +1,29 @@
+//If Else
+
+#include <stdio.h>
+
+int main()
+{
+
+    int idade;
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    if (idade < 12)
+    {
+        printf("Criança");
+    }
+    else if (idade >= 12 && idade < 18)
+    {
+        printf("Adolescente");
+    }
+    else if (idade >= 18 && idade < 60)
+    {
+        printf("Adulto");
+    }
+    else
+    {
+        printf("Idoso");
+    }
+    return 0;
+}
