0ab47b742d919884692cd667db75ea8cc400513b
Alex Muller <alexmullercarvalho@gmail.com>
2026-03-17 10:30:14 -0300

    add c exercise ex016 recursive

---
+++ c/ex016.c
@@ -0,0 +1,24 @@
+//Recursive
+
+#include <stdio.h>
+
+void recursivo(int numero)
+{
+    if (numero > 0)
+    {
+        printf("%d \n", numero);
+        recursivo(numero - 1);
+    }
+}
+
+
+int main()
+{
+
+    int quantidade = 10;
+
+    printf("Contagem Regressiva... \n");
+    recursivo(quantidade);
+
+    return 0;
+}
