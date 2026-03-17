bd0040a8fb696361be0c1ee7fc0f4a019460ec47
Alex Muller <alexmullercarvalho@gmail.com>
2026-03-16 21:24:12 -0300

    add c exercise ex009 loop for

---
+++ c/ex009.c
@@ -0,0 +1,18 @@
+//Loop For
+
+#include <stdio.h>
+
+int main()
+{
+    int num, i;
+
+    printf("Digite um número para calcularmos a tabuada: \n");
+    scanf("%d", &num);
+
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d x %d = %d \n", i, num, i * num);
+    }
+
+    return 0;
+}
